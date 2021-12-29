da44716f16bee71be484e00b9ac09bc0e4d78ea2
mijin <mmcho122@gmail.com>
2021-12-29 13:42:24 +0900

    💄 Header style 적용

--- src/components/CoffeeHeader.jsx
+++ src/components/CoffeeHeader.jsx
@@ -1,11 +1,44 @@
 import React from "react"
+// 자동 임포트
+import styled from "styled-components";
 
 const CoffeeHeader = () => {
     return (
-        <div>
-            CoffeeHeader
-        </div>
-    )
-}
+        // <header>
+        <HeaderCont>
+            <HeaderTitle className={styled.title}>Coffee Site</HeaderTitle>
+            <HeaderNav>
+                <HeaderMenu href="#none">Iced</HeaderMenu>
+                <HeaderMenu href="#none">Hot</HeaderMenu>
+            </HeaderNav>
+        </HeaderCont>
+    );
+};
 
-export default CoffeeHeader
+export default CoffeeHeader;
+
+//스타일 컴포넌트
+const HeaderCont = styled.header`
+    width: 100%;
+    height: 70px;
+    background-color: #333;
+    color: beige;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: auto 100px;
+`
+
+const HeaderTitle = styled.h1`
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 50px;
+`
+const HeaderNav = styled.nav`
+    margin-right: 20px;
+    font-size: 20px;
+`
+
+const HeaderMenu = styled.a`
+    margin-left: 10px;
+`
